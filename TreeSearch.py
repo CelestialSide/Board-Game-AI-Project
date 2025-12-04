d5f8320272f592e76ebe0d8c81d8b1add712e6ec
LindseyRedepenning <redepenningl6964@my.uwstout.edu>
2025-12-04 15:44:30 -0600

    Tree search with othello

--- TreeSearch.py
+++ TreeSearch.py
@@ -1,173 +1,111 @@
 import random
 from copy import copy
-
+import Board as b
+import Othello as o
 import numpy as np
+from tqdm import tqdm
 
-class TreeNode:
-    def __init__(self, data, parent = None, action = None):
-        self.state = data
+def random_game(white = 34628173824, black = 68853694464, turn_count = 0):
+    if turn_count % 2 == 0: player, opponent = black, white
+    else: player, opponent = white, black
 
-        self.children = []
-        self.parent = parent
-        self.action = action
-        self.untried_actions = open_spots(data)
+    last_turn_pass = False
+    while True:
+        valid_moves = o.get_valid_move_list(player, opponent)
+        if len(valid_moves) == 0: chosen_move = -1
+        else: chosen_move = random.choice(valid_moves)
 
-        self.score = 0
-        self.traversals = 0
+        if chosen_move == -1:
+            if last_turn_pass:
+                # Game complete! Parsing who is black currently
+                #   Returns: White Board, Black Board
+                if turn_count % 2 == 0:
+                    return o.determine_winner(opponent, player)[1]
+                else:
+                    return o.determine_winner(player, opponent)[1]
+            else:
+                last_turn_pass = True
+        else:
+            player, opponent = o.update_board(chosen_move, player, opponent)
+            last_turn_pass = False
 
-    def add_traversal(self):
-        self.traversals += 1
+        player, opponent = opponent, player
+        turn_count += 1
 
-    def add_child(self, child_node):
-        child_node.parent = self
-        self.children.append(child_node)
 
-    def get_level(self):
-        level = 0
-        p = self.parent
-        while p:
-            level += 1
-            p = p.parent
-        return level
+class MonteCarlo:
+    def __init__(self, root):
+        self.root = root
+    def get_best_child(self, node):
+        while node.is_explored():
+            utc = [(child, child.compute_UCT(c = 2)) for child in node.children]
+            max_utc = max([child[1] for child in utc])
+            selected_node = random.choice([child for child in utc if child[1] == max_utc])[0]
+            node = selected_node
+        return node
 
-    def get_current_player(self):
-        return 2 if self.state.count(1) > self.state.count(2) else 1
-
-    def print_tree(self):
-        prefix = "  " * self.get_level() + "|-- " if self.parent else ""
-        print(prefix + str(self.state) + f" n: {self.traversals}, t: {self.score}")
-        if self.children:
-            for child in self.children:
-                child.print_tree()
-
-    def expand(self):
-        """Add one of the remaining actions as a child."""
-        picked_move = self.untried_actions.pop()
-        new_state = copy(self.state)
-        player = self.get_current_player()
-        new_state[picked_move] = player
-        child = TreeNode(new_state, parent=self, action=picked_move)
-        self.children.append(child)
+    def expand(self, parent):
+        child = parent.make_child()
         return child
 
-    def game_over(self, state = None):
-        used_state = self.state if not state else state
-        prev_spot = used_state[0]
-        for i in range(1, len(used_state)):
-            if not prev_spot == 0 and used_state[i] == prev_spot:
-                return used_state[i]
-            else:
-                prev_spot = used_state[i]
-        if len(self.untried_actions) == 0:
-            return -1
-        return 0
-
-    def best_child(self, const = 2):
-        if self.children:
-            best_child = self.children[0]
-            for c in self.children:
-                BUCB = best_child.score / best_child.traversals + const * np.sqrt(
-                    np.log(self.traversals) / best_child.traversals)
-                CUCB = c.score / c.traversals + const * np.sqrt(np.log(self.traversals) / c.traversals)
-                if CUCB > BUCB:
-                    best_child = c
-
-            return best_child
-        return self
-
-    def rollout(self):
+    def rollout(self, node, reward=1):
         """Play random moves until the game ends."""
-        state = copy(self.state)
-        player = self.get_current_player()
+        result = random_game(node.white, node.black, node.turn_count)
+        if result > 0:
+            return reward
+        elif result < 0:
+            return -1 * reward
+        else:
+            return 0
 
-        while True:
-            winner = self.game_over(state = state)
-            if not winner == 0 and not winner == -1:
-                return -1 if winner == 1 else 1
+    def backpropagation(self, node, score):
+        while node.parent is not None:
+            if node.to_play:
+                node.score += score
+            else:
+                node.score += -score
+            node.visits += 1
+            node = node.parent
 
-            actions = open_spots(state)
-            if not actions:
-                return 0  # Draw
+def monte_carlo_tree_search(root=None, iterations=1000):
+    if root is None:
+        true_root = b.Node()
+        true_root.visits = 1
+        tree = MonteCarlo(true_root)
+    else:
+        if root.visits < 1: root.visits = 1
+        tree = MonteCarlo(root)
 
-            rand_move = random.choice(actions)
-            state[rand_move] = player
-            player = 1 if player == 2 else 2
+    progress_bar = tqdm(range(iterations))
+    for iteration in progress_bar:
+        node = tree.get_best_child(tree.root)
+        child = tree.expand(node)
+        score = tree.rollout(child)
+        tree.backpropagation(child, score)
 
-    def backpropagate(self, result):
-        self.traversals += 1
-        self.score += result
-        if self.parent:
-            self.parent.backpropagate(result)
-
-
-def MCTS(board, iterations=100) -> int:
-    root = TreeNode(board)
-
-    for i in range(iterations):
-        node = root
-        current_game_state = node.game_over()
-
-        # Will find the best child node as long as the game is still possible
-        while current_game_state == 0 and len(node.untried_actions) == 0:
-            node = node.best_child()
-
-        # If the game has not ended, the best node is then explored.
-        current_game_state = node.game_over()
-        if current_game_state == 0:
-            node = node.expand()
-
-        result = node.rollout()
-
-        node.backpropagate(result)
-
-    return root.best_child(const=0).action
-
-def score_board(board_state):
-    if board_state == -1 or board_state == 0:
-        return 0
-
-    return -(board_state * 2 - 3)
-
-def open_spots(board):
-    open_spots = []
-    for i in range(len(board)):
-        if board[i] == 0:
-            open_spots.append(i)
-
-    return open_spots
-
-def ask_for_move(board) -> int:
-    print("Input move location(ex. 0 for the first spot): ")
-    user_input = input()
-    poss_moves = open_spots(board)
-    while not validate_input(user_input, poss_moves):
-        print("Input move location(ex. 0 for the first spot): ")
-        user_input = input()
-
-    return int(user_input)
-
-def validate_input(i, pm):
-    for m in pm:
-        if i == str(m):
-            return True
-    return False
+        if (iteration + 1) % 100 == 0:
+            progress_bar.set_postfix({'Top Move': tree.root.compute_best_score()[1]})
+    return tree.root.compute_best_score()
 
 if __name__ == "__main__":
-    game_board = [0,0,0,0]
-    root = TreeNode(game_board)
-    game_state = root.game_over()
-    p = 1
-    while game_state == 0:
-        root = TreeNode(game_board)
-        if p == 1:
-            move = ask_for_move(game_board)
-        else:
-            move = MCTS(game_board)
+    # Test Code running through 2 MCTS moves
+    white, black = 34628173824, 68853694464
+    node, move = monte_carlo_tree_search()
+    black, white = o.update_board(move, black, white)
+    #o.disp_game(white, black, False)
 
-        game_board[move] = p
+    valid_moves = o.get_valid_move_list(white, black)
+    move = random.choice(valid_moves)
+    white, black = o.update_board(move, white, black)
+    #o.disp_game(white, black, True)
 
-        root.print_tree()
-        print(game_board)
+    if move in node.available_moves:
+        new_root = node.make_child(move)
+    else:
+        for child in node.children:
+            if child.move == move:
+                new_root = child
+                break
 
-        p = 3 - p
-        game_state = root.game_over()
+    node, move = monte_carlo_tree_search(new_root)
+    black, white = o.update_board(move, black, white)
