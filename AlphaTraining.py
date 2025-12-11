85a515074956526157a70dc3292703b8256b3de8
Nathan LaCrosse <mancool5672@gmail.com>
2025-12-11 10:11:01 -0600

    Trained overnight.

--- AlphaTraining.py
+++ AlphaTraining.py
@@ -29,10 +29,15 @@ def train(network : AlphaZeroNet, play_dat : PlayDataset, batch_size, epochs, ep
     dat = play_dat
 
     for epoch in range(epochs):
-        # First, train based off of the current dataset.
+        # Update the dataset by playing games and resave
+        if not pre_train_policy_only and epoch % epochs_per_play == 0:
+            dat.play_games(net.eval().cpu(), games_per_epoch, mcts_steps_per_turn)
+            dat.save_as(dat_save_path)
+
+        # Train based off of the current dataset.
         dat_loader = DataLoader(dat, batch_size, shuffle=True)
         p_bar = tqdm(dat_loader, desc=f"Epoch {epoch+1}")
-        net = net.train()
+        net = net.train().to(device)
 
         for _, batch in enumerate(p_bar):
             state, policy, value = batch
@@ -43,10 +48,16 @@ def train(network : AlphaZeroNet, play_dat : PlayDataset, batch_size, epochs, ep
 
             # Compute policy loss -> comparing policy distributions (slightly different that KL-Div)
             log_probs = torch.log_softmax(pred_p, dim=1)
-            policy_loss = -(policy * log_probs).sum(dim=1).mean()
+
+            policy_target = policy.to(device)
+            # Add label smoothing to pre-training
+            if pre_train_policy_only:
+                policy_target = policy_target * (1 - 0.1) + 0.1 / policy_target.size(1)
+
+            policy_loss = -(policy.to(device) * log_probs).sum(dim=1).mean()
 
             # Compute value loss -> how good is the current state?
-            value_loss = value_loss_fn(pred_v, value)
+            value_loss = value_loss_fn(pred_v, value.to(device))
 
             # Combined loss is both policy and value loss.
             loss = policy_loss + value_loss
@@ -56,11 +67,14 @@ def train(network : AlphaZeroNet, play_dat : PlayDataset, batch_size, epochs, ep
             optimizer.step()
 
             p_bar.set_postfix({'loss:' : loss.item(), 'policy_loss:' : policy_loss.item(), 'value_loss' : value_loss.item()})
+        
 
-        # Now, update the dataset by playing games. Save the new dataset and network after for later use.
-        if not pre_train_policy_only:
-            dat.play_games(net.eval(), games_per_epoch, mcts_steps_per_turn)
-            dat.save_as(dat_save_path)
+            if (epoch + 1) % (epochs_per_play*5) == 0:
+                try:
+                    torch.save(net.state_dict(), net_save_path[:-3] + f'vers{(epoch+1) // (epochs_per_play*5)}' + '.pt')
+                    dat.save_as(dat_save_path[:-5] + f'vers{(epoch+1) // (epochs_per_play*5)}' + '.json')
+                except:
+                    pass
         torch.save(net.state_dict(), net_save_path)
 
     # For unfreezing value head
@@ -79,7 +93,9 @@ def train(network : AlphaZeroNet, play_dat : PlayDataset, batch_size, epochs, ep
 
 if __name__ == '__main__':
     net = AlphaZeroNet()
-    # net.load_state_dict(torch.load('Models/zero.pt'))
-    dat = PlayDataset('Data/expert_start.json', max_buffer_size=130000)
+    net.load_state_dict(torch.load('Models/zero.pt'))
+    dat = PlayDataset('Data/zero_first.json', max_buffer_size=150000, pre_load_cap=-1)
+    # net = train(net, dat, batch_size=512, epochs=3, epochs_per_play=5, lr=3e-4, games_per_epoch=100, net_save_path="Models/pre_trained.pt", pre_train_policy_only=True)
 
-    train(net, dat, 128, 100, 3, 3e-4, games_per_epoch=100)
+    # dat = PlayDataset('Data/expert_start.json', max_buffer_size=150000, pre_load_cap=50000)
+    train(net, dat, batch_size=128, epochs=100, epochs_per_play=3, lr=3e-4, games_per_epoch=100)
